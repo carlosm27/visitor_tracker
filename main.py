15f2ccc73e89b8780003e2a1f35793348acdb9ec
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-06-25 18:37:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from datetime import datetime
 from tracker import visitor_tracker
+from controllers import new_log, all_logs
 
 app = FastAPI()
 
@@ -8,10 +9,23 @@ app = FastAPI()
 @app.middleware("tracker")
 async def tracker(request: Request, call_next):
     tracker = visitor_tracker(request)
-    response = await call_next(request)
     print(tracker)
+    
+    log = new_log(tracker["ip_address"], tracker["request_url"], tracker["request_port"],
+                      tracker["request_path"], tracker["request_method"],
+                      tracker["browser_type"], tracker["operating_system"],tracker["request_time"])
+    print(log)
+    
+        
+    
+    response = await call_next(request)
     return response
 
+    
+@app.get("/logs")
+def logs():
+    logs = all_logs()
+    return logs
 
 @app.get("/")
 def index():
@@ -28,4 +42,4 @@ def some_func():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5000)
