5bd5b570cf105386bba5625b42266ac63487cf2f
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-06-25 14:12:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,27 +14,16 @@ async def tracker(request: Request, call_next):
 
 
 @app.get("/")
-def index(request: Request):
-    """Track website visitor."""
-    ip_address = request.client.host
-    request_url = request.url._url
-    request_port = request.url.port
-    request_path = request.url.path
-    request_method = request.method
-    request_time = datetime.now()
-    browser_type = request.headers["User-Agent"]
-    operating_system = request.headers["Accept"]
+def index():
+    return "Hello, world"
 
+@app.get("/json")
+def some_func():
     return {
-        "ip_address": ip_address,
-        "request_url": request_url,
-        "request_port": request_port,
-        "request_path": request_path,
-        "request_method": request_method,
-        "request_time": request_time,
-        "browser_type": browser_type,
-        "operating_system": operating_system,
+        "some_json": "Some Json"
     }
+
+
 
 
 
