4fc3c6b781439098e61c64fd26fabb5c57e67ae9
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-06-29 22:22:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,32 @@ from datetime import datetime
 from tracker import visitor_tracker
 from controllers import new_log, all_logs
 
+import pika
+
+
+
+
 app = FastAPI()
 
 
 @app.middleware("tracker")
 async def tracker(request: Request, call_next):
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+
+
+    channel.queue_declare(queue='logs')
+
     tracker = visitor_tracker(request)
     print(tracker)
     
     log = new_log(tracker["ip_address"], tracker["request_url"], tracker["request_port"],
                       tracker["request_path"], tracker["request_method"],
                       tracker["browser_type"], tracker["operating_system"],tracker["request_time"])
-    print(log)
+    
+    channel.basic_publish(exchange='', routing_key='hello', body=log)
+    print(" [x] Sent 'Logs'")
+    connection.close()
     
         
     
