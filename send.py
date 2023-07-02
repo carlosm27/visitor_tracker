d1e201095f402012484c8f68ee065e5bcf461300
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-07-01 20:16:45 -0400

    Update send.py

--- send.py
+++ send.py
@@ -6,6 +6,6 @@ def sender(body: dict):
     channel = connection.channel()
     channel.queue_declare(queue='logs')
 
-    channel.basic_publish(exchange='', routing_key='hello', body=body)
+    channel.basic_publish(exchange='', routing_key='logs', body=body)
     print(" [x] Sent 'Logs'")
     connection.close()
